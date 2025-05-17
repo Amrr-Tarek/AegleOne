50b7bf838a41f799a22dbbe625e4e8d01ea8d155
Amr Tarek <148470882+Amrr-Tarek@users.noreply.github.com>
2025-05-18 00:46:40 +0300

    hosted db on azure

--- backend/aegleone/aegleone/settings.py
+++ backend/aegleone/aegleone/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles" # Uncomment upon production
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
